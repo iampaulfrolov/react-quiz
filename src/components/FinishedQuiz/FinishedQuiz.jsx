a789b275e4d12efca224ebe4900fffb00fb7bdc0
paulfrolov <pavlo.frolov@nure.ua>
2021-03-19 00:18:33 +0200

    Bug fix

--- src/components/FinishedQuiz/FinishedQuiz.jsx
+++ src/components/FinishedQuiz/FinishedQuiz.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import c from './FinishedQuiz.module.css'
 import Button from '../UI/Button/Button'
+import { Link } from 'react-router-dom'
 
 const FinishedQuiz = props => {
   const successCount = Object.keys(props.results).reduce((total, key) => {
@@ -35,7 +36,9 @@ const FinishedQuiz = props => {
 
       <div>
         <Button onClick={props.onRetry} type="primary">Repeat</Button>
-        <Button type="success">Go to the test list</Button>
+        <Link to='/'>
+          <Button type="success">Go to the test list</Button>
+        </Link>
       </div>
     </div>
   )
